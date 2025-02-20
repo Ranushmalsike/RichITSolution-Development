ff5ab3a10eda3db2289fcc0964338b9fc4447d3d
Ranushmalsike <ranushmalsika@gmail.com>
2025-02-20 15:43:03 +0530

    dev 200225

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
         <Route path="/" element={
           <>
             
-              <div className="">
+              <div className="mt-10">
                 <CustomCarousel />
               </div>
             
